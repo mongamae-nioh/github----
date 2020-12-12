490928e7bdc1a156780050e69b8ad2fb467a9fc8
もんがまえ仁王 <jkawaoka@gmail.com>
2020-12-12 20:45:45 +0900

    Update variables.tf
    https://learn.hashicorp.com/tutorials/terraform/aws-variables
    のRich data typesに合わせてtypeの表記を修正

--- variables.tf
+++ variables.tf
@@ -3,27 +3,23 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnets_cidr" {
-  type    = list(string)
   default = ["n.n.n.n/nn", "n.n.n.n/nn"]
 }
 
 variable "private_subnets_cidr" {
-  type    = list(string)
   default = ["n.n.n.n/nn", "n.n.n.n/nn"]
 }
 
 variable "azs" {
-  type    = list(string)
   default = ["ap-northeast-1a", "ap-northeast-1c"]
 }
 
 variable "gip" {
-  type    = list(string)
   default = ["nnn.nnn.nnn.nnn/32", "nnn.nnn.nnn.nnn/32"]
 }
 
 variable "tags" {
-  type = map(string)
+  type = map
   default = {
     Name  = "test"
   }
